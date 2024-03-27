beac9cb9d66cd9e8ec739daea362c5e5196582e6
lichen-1997 <lichenyx@vip.qq.com>
2024-03-27 23:16:22 +0800

    add spliter

--- MyCloudDrive/src/fend/uiMain/MainWidget.cpp
+++ MyCloudDrive/src/fend/uiMain/MainWidget.cpp
@@ -4,6 +4,8 @@ MainWidget::MainWidget(QWidget *parent)
 	: QWidget(parent)
 {
 	ui.setupUi(this);
+	ui.splitter->setStretchFactor(0, 1);
+	ui.splitter->setStretchFactor(1, 4);
 }
 
 MainWidget::~MainWidget()
